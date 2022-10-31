c847aecb03d47ffe90a4fa41cb2859e4ebd9f675
M Alvin Syahrin <alvin.syahrin0795@gmail.com>
2022-10-31 21:34:25 +0700

    Add project files.

---
+++ ElasticWebApi/Controllers/ProductsController.cs
@@ -0,0 +1,86 @@
+﻿using ElasticWebApi.Model.Product;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Nest;
+
+namespace ElasticWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IElasticClient _elasticClient;
+        private readonly ILogger<ProductsController> _logger;
+
+        public ProductsController(
+                        IElasticClient elasticClient,
+                        ILogger<ProductsController> logger)
+        {
+            _logger = logger;
+            _elasticClient = elasticClient;
+        }
+
+        [HttpGet(Name ="search")]
+        public async Task<IActionResult> Search([FromQuery]string keyword)
+        {
+            var result = await _elasticClient.SearchAsync<Product>(
+                             s => s.Query(
+                                 q => q.QueryString(
+                                     d => d.Query('*' + keyword + '*')
+                                 )).Size(5000));
+
+            _logger.LogInformation("ProductsController Get - ", DateTime.UtcNow);
+            return Ok(result.Documents.ToList());
+        }
+
+        [HttpPost(Name ="create")]
+        public async Task<IActionResult> Create([FromBody]Product product)
+        {
+            // Add product to ELS index
+            var product1 = new Product
+            {
+                Description = "Product 1",
+                Id = Guid.NewGuid(),
+                Price = 200,
+                Measurement = "40",
+                Quantity = 43888200,
+                Title = "Nike Shoes",
+                Unit = "10",
+                CreatedAt = new DateTime().AddHours(1)
+            };
+
+            var product2 = new Product
+            {
+                Description = "Product 2",
+                Id = Guid.NewGuid(),
+                Price = 30000000,
+                Measurement = "38",
+                Quantity = 299,
+                Title = "Adidas Shoes",
+                Unit = "10",
+                CreatedAt = new DateTime().AddHours(1)
+            };
+
+            var product3 = new Product
+            {
+                Description = "Product 3",
+                Id = Guid.NewGuid(),
+                Price = 2000000,
+                Measurement = "30",
+                Quantity = 92,
+                Title = "Delicio Shoes",
+                Unit = "10",
+                CreatedAt = new DateTime().AddHours(2)
+            };
+
+            // Index product dto
+            await _elasticClient.IndexDocumentAsync(product1);
+            await _elasticClient.IndexDocumentAsync(product2);
+            await _elasticClient.IndexDocumentAsync(product3);
+
+            _logger.LogInformation("ProductsController Get - ", DateTime.UtcNow);
+            return Ok();
+        }
+
+    }
+}
---
+++ ElasticWebApi/Model/Product/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ElasticWebApi.Model.Product
+{
+    public class Product
+    {
+
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public string? Measurement { get; set; }
+        public string? Title { get; set; }
+        public string? Unit { get; set; }
+        
+        [JsonIgnore]
+        public DateTime CreatedAt { get; set; }
+    }
+}
